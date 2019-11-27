c29c3420081c250ac4ddcebb3c39e7e2dc63e028
jiayu-joyce-he <jiayuhe@alumni.stanford.edu>
2019-11-26 16:14:58 -0800

    finish database

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :bikes
+  has_many :apartments
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "ml5403941@gmail.com", password: "hejiayu21")
 
-Apartment.create(street_a: "J street", street_b: "11th Broadway", city: "San Diego", zipcode: "92126", state: "CA", country: "USA", contact_name: "John Doe", contact_number: "123-456-789", contact_hour: "Afternoon")
+User.create(email: "hello@world.edu", password: "123123123123")
 
-Apartment.create(street_a: "X street", street_b: "1th Broadway", city: "San Diego", zipcode: "92126", state: "CA", country: "USA", contact_name: "John Doe", contact_number: "123-456-789", contact_hour: "Afternoon")
+Apartment.create(user_id: 1, street_a: "J street", street_b: "11th Broadway", city: "San Diego", zipcode: "92126", state: "CA", country: "USA", contact_name: "John Doe", contact_number: "123-456-789", contact_hour: "Afternoon")
 
-Apartment.create(street_a: "B street", street_b: "10th Broadway", city: "San Diego", zipcode: "92126", state: "CA", country: "USA", contact_name: "Jane Doe", contact_number: "123-456-789", contact_hour: "24/7")
+Apartment.create(user_id: 2, street_a: "X street", street_b: "1th Broadway", city: "San Diego", zipcode: "92126", state: "CA", country: "USA", contact_name: "John Doe", contact_number: "123-456-789", contact_hour: "Afternoon")
+
+Apartment.create(user_id: 2, street_a: "B street", street_b: "10th Broadway", city: "San Diego", zipcode: "92126", state: "CA", country: "USA", contact_name: "Jane Doe", contact_number: "123-456-789", contact_hour: "24/7")
